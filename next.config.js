f15a1792d13a5a5bde1526810eb698961853bec2
souro-2002 <sourochatterjee92@gmail.com>
2023-06-13 11:59:04 +0530

    Made Better

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash:true,
-  images:{unoptimized:true}
+  trailingSlash:true
+  // images:{unoptimized:true}
 }
 
 module.exports = nextConfig
