56015a5db0031462d3976537a9e11ec99e342788
souro-2002 <sourochatterjee92@gmail.com>
2023-06-14 19:42:45 +0530

    Analytics Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Navbar/>
     <Component {...pageProps} />
+    <Analytics/>
   </>
 }
